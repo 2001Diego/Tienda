84b901cbbcf3c7e21ed14ee22c9a0762b13dc830
Diego Segura Vega <Diego Segura Vega@LAPTOP-TTDFIE71>
2022-07-25 19:44:37 -0600

    Agregamos los temas finales del curso, incluyendo securidad y autenticacion.

--- src/main/java/com/tienda/repository/PersonaRepository.java
+++ src/main/java/com/tienda/repository/PersonaRepository.java
@@ -21,7 +21,7 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface PersonaRepository extends CrudRepository<Persona,Long> {
-
+    Persona findByNombre (String nombre);
     public void deleteById(long id);
 
     
